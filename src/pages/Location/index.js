681e443e0d3c2acee1c7a6bc39f09da99852a6ac
Monika Teiserskyte <moni6740@stud.kea.dk>
2023-05-24 23:19:35 +0200

    init

---
+++ src/pages/Location/index.js
@@ -0,0 +1,3 @@
+//  Follow the manuals for google maps integration
+// NPM: https://www.npmjs.com/package/@googlemaps/js-api-loader
+// Steps: https://www.99darshan.com/posts/interactive-maps-using-nextjs-and-google-maps/
