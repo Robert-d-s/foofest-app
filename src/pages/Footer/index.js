7e62e6a67af73618663cb6bb2d52558d42921453
Monika Teiserskyte <moni6740@stud.kea.dk>
2023-05-25 15:44:35 +0200

    its moving

--- src/pages/Footer/index.js
+++ src/pages/Footer/index.js
@@ -2,3 +2,26 @@ import styles from "./footer.module.css";
 
 // Footer with infinite marquee (sponsors)
 // Trying out the sample w/o images first
+
+export default function Footer() {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.marquee}>
+        <div className={`${styles.marqueeContent} ${styles.scroll}`}>
+          <p>one 1</p>
+          <p>two 2</p>
+          <p>three 3</p>
+          <p>four 4</p>
+          <p>five 5</p>
+        </div>
+        <div className={`${styles.marqueeContent} ${styles.scroll}`}>
+          <p>six 6</p>
+          <p>seven 7</p>
+          <p>eight 8</p>
+          <p>nine 9</p>
+          <p>ten 10</p>
+        </div>
+      </div>
+    </div>
+  );
+}
