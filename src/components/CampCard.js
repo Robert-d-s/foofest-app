3ddb9618bd8c107363aa384094144c25e09accca
teriTheDesigner <tere1109@stud.kea.dk>
2023-05-25 21:01:24 +0200

    not going to merge

--- src/components/CampCard.js
+++ src/components/CampCard.js
@@ -10,6 +10,9 @@ const CampCard = ({ spot, formData, handleCampSpotChange }) => {
           value={spot.area}
           checked={formData.campData.campSpot === spot.area}
           onChange={handleCampSpotChange}
+          disabled={
+            spot.available < formData.ticketData.ticketQuantity ? true : false
+          }
         />
         {spot.area}
       </label>
