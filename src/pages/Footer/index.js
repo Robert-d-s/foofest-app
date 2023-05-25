90ad3288e106880ffbd87f64dfff5c038758ef68
Monika Teiserskyte <moni6740@stud.kea.dk>
2023-05-25 18:21:34 +0200

    ready to be merged and turned into component

--- src/pages/Footer/index.js
+++ src/pages/Footer/index.js
@@ -2,11 +2,11 @@ import styles from "./footer.module.css";
 import Image from "next/image";
 
 // Footer with infinite marquee (sponsors)
-// Trying out the sample w/o images first
+// when ready, add the images or svg
 
 export default function Footer() {
   return (
-    <div className={styles.Mwrapper}>
+    <div className={styles.marqueeWrapper}>
       <div className={styles.marquee}>
         <div className={styles.marqueeContent}>
           <p>one1</p>
@@ -34,21 +34,5 @@ export default function Footer() {
         </div>
       </div>
     </div>
-    // <section className={styles.marquee}>
-    //   <div className={`${styles.marqueeContent} ${styles.scroll}`}>
-    //     <p>one 1</p>
-    //     <p>two 2</p>
-    //     <p>three 3</p>
-    //     <p>four 4</p>
-    //     <p>five 5</p>
-    //   </div>
-    //   <div className={`${styles.marqueeContent} ${styles.scroll}`}>
-    //     <p>six 6</p>
-    //     <p>seven 7</p>
-    //     <p>eight 8</p>
-    //     <p>nine 9</p>
-    //     <p>ten 10</p>
-    //   </div>
-    // </section>
   );
 }
