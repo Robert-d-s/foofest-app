8d8a854f4ded98af592a53cdb97beec4d0b3ac40
Monika Teiserskyte <moni6740@stud.kea.dk>
2023-05-25 17:27:11 +0200

    testing...

--- src/pages/Footer/index.js
+++ src/pages/Footer/index.js
@@ -6,23 +6,37 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <div className={styles.marqueeWrapper}>
-      <section className={styles.marquee}>
-        <div className={`${styles.marqueeContent} ${styles.scroll}`}>
-          <p>one 1</p>
-          <p>two 2</p>
-          <p>three 3</p>
-          <p>four 4</p>
-          <p>five 5</p>
-        </div>
-        <div className={`${styles.marqueeContent} ${styles.scroll}`}>
-          <p>six 6</p>
-          <p>seven 7</p>
-          <p>eight 8</p>
-          <p>nine 9</p>
-          <p>ten 10</p>
-        </div>
-      </section>
+    <div className={styles.marquee}>
+      <div className={styles.marqueeContent}>
+        <p>one1</p>
+        <p>two 2</p>
+        <p>three 3</p>
+        <p>four 4</p>
+        <p>five 5</p>
+      </div>
+      <div className={styles.marqueeContent}>
+        <p>six 6</p>
+        <p>seven 7</p>
+        <p>eight 8</p>
+        <p>nine 9</p>
+        <p>ten 10</p>
+      </div>
     </div>
+    // <section className={styles.marquee}>
+    //   <div className={`${styles.marqueeContent} ${styles.scroll}`}>
+    //     <p>one 1</p>
+    //     <p>two 2</p>
+    //     <p>three 3</p>
+    //     <p>four 4</p>
+    //     <p>five 5</p>
+    //   </div>
+    //   <div className={`${styles.marqueeContent} ${styles.scroll}`}>
+    //     <p>six 6</p>
+    //     <p>seven 7</p>
+    //     <p>eight 8</p>
+    //     <p>nine 9</p>
+    //     <p>ten 10</p>
+    //   </div>
+    // </section>
   );
 }
