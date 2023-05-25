2c3e56be975fb375d7ce26bb3985e97f10024046
Monika Teiserskyte <moni6740@stud.kea.dk>
2023-05-25 18:12:27 +0200

    it WORKS

--- src/pages/Footer/index.js
+++ src/pages/Footer/index.js
@@ -6,20 +6,32 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <div className={styles.marquee}>
-      <div className={styles.marqueeContent}>
-        <p>one1</p>
-        <p>two 2</p>
-        <p>three 3</p>
-        <p>four 4</p>
-        <p>five 5</p>
-      </div>
-      <div className={styles.marqueeContent}>
-        <p>six 6</p>
-        <p>seven 7</p>
-        <p>eight 8</p>
-        <p>nine 9</p>
-        <p>ten 10</p>
+    <div className={styles.Mwrapper}>
+      <div className={styles.marquee}>
+        <div className={styles.marqueeContent}>
+          <p>one1</p>
+          <p>two 2</p>
+          <p>three 3</p>
+          <p>four 4</p>
+          <p>five 5</p>
+          <p>six 6</p>
+          <p>seven 7</p>
+          <p>eight 8</p>
+          <p>nine 9</p>
+          <p>ten 10</p>
+        </div>
+        <div aria-hidden="true" className={styles.marqueeContent}>
+          <p>one1</p>
+          <p>two 2</p>
+          <p>three 3</p>
+          <p>four 4</p>
+          <p>five 5</p>
+          <p>six 6</p>
+          <p>seven 7</p>
+          <p>eight 8</p>
+          <p>nine 9</p>
+          <p>ten 10</p>
+        </div>
       </div>
     </div>
     // <section className={styles.marquee}>
