7c07c34f204f9db01ece33b71edb18cc797a97c9
Robert Stoica <robe3750@stud.kea.dk>
2023-05-26 08:19:56 +0200

    serversideprops

--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -1,38 +1,92 @@
-// MainPage.js
-import React, { useState, useEffect } from "react";
+// // MainPage.js
+// import React, { useState, useEffect } from "react";
+// import DaySelector from "../components/DaySelector";
+// import Stage from "../components/Stage";
+// import Band from "../components/Band";
+// import Modal from "../components/Modal";
+
+// const MainPage = () => {
+//   const [bands, setBands] = useState([]);
+//   const [schedule, setSchedule] = useState({});
+//   const [selectedDay, setSelectedDay] = useState("mon");
+//   const [loading, setLoading] = useState(true);
+//   const [selectedBand, setSelectedBand] = useState(null);
+//   const [isModalOpen, setIsModalOpen] = useState(false);
+
+//   useEffect(() => {
+//     const fetchBandsAndSchedule = async () => {
+//       const bandsResponse = await fetch(
+//         "https://hollow-glowing-gladiolus.glitch.me/bands"
+//       );
+//       const bandsData = await bandsResponse.json();
+
+//       const scheduleResponse = await fetch(
+//         "https://hollow-glowing-gladiolus.glitch.me/schedule"
+//       );
+//       const scheduleData = await scheduleResponse.json();
+//       // const scheduleData = await scheduleResponse.json();
+
+//       setBands(bandsData);
+//       setSchedule(scheduleData);
+//       setLoading(false);
+//     };
+
+//     fetchBandsAndSchedule();
+//   }, []);
+
+//   const handleDayChange = (day) => {
+//     setSelectedDay(day);
+//   };
+
+//   const handleBandClick = (band) => {
+//     setSelectedBand(band);
+//     setIsModalOpen(true);
+//   };
+
+//   const closeModal = () => {
+//     setIsModalOpen(false);
+//   };
+
+//   if (loading) {
+//     return <div>Loading...</div>;
+//   }
+
+//   return (
+//     <div>
+//       <DaySelector onDayChange={handleDayChange} />
+//       {Object.entries(schedule).map(([stage, days]) => (
+//         <Stage key={stage} stage={stage} days={days} day={selectedDay} />
+//       ))}
+//       {bands.map((band) => (
+//         <Band
+//           key={band.id}
+//           band={band}
+//           onBandClick={handleBandClick}
+//           selectedDay={selectedDay}
+//           schedule={schedule}
+//         />
+//       ))}
+//       {isModalOpen && selectedBand && (
+//         <Modal band={selectedBand} onClose={closeModal} />
+//       )}
+//     </div>
+//   );
+// };
+
+// export default MainPage;
+
+import React, { useState } from "react";
 import DaySelector from "../components/DaySelector";
 import Stage from "../components/Stage";
 import Band from "../components/Band";
 import Modal from "../components/Modal";
 
-const MainPage = () => {
-  const [bands, setBands] = useState([]);
-  const [schedule, setSchedule] = useState({});
+const MainPage = ({ bandsData, scheduleData }) => {
+  const [bands, setBands] = useState(bandsData);
+  const [schedule, setSchedule] = useState(scheduleData);
   const [selectedDay, setSelectedDay] = useState("mon");
-  const [loading, setLoading] = useState(true);
   const [selectedBand, setSelectedBand] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
-
-  useEffect(() => {
-    const fetchBandsAndSchedule = async () => {
-      const bandsResponse = await fetch(
-        "https://hollow-glowing-gladiolus.glitch.me/bands"
-      );
-      const bandsData = await bandsResponse.json();
-
-      const scheduleResponse = await fetch(
-        "https://hollow-glowing-gladiolus.glitch.me/schedule"
-      );
-      const scheduleData = await scheduleResponse.json();
-      // const scheduleData = await scheduleResponse.json();
-
-      setBands(bandsData);
-      setSchedule(scheduleData);
-      setLoading(false);
-    };
-
-    fetchBandsAndSchedule();
-  }, []);
 
   const handleDayChange = (day) => {
     setSelectedDay(day);
@@ -46,10 +100,6 @@ const MainPage = () => {
   const closeModal = () => {
     setIsModalOpen(false);
   };
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
 
   return (
     <div>
@@ -72,5 +122,19 @@ const MainPage = () => {
     </div>
   );
 };
+
+export async function getServerSideProps() {
+  const bandsResponse = await fetch(
+    "https://hollow-glowing-gladiolus.glitch.me/bands"
+  );
+  const bandsData = await bandsResponse.json();
+
+  const scheduleResponse = await fetch(
+    "https://hollow-glowing-gladiolus.glitch.me/schedule"
+  );
+  const scheduleData = await scheduleResponse.json();
+
+  return { props: { bandsData, scheduleData } };
+}
 
 export default MainPage;
