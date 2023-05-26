fc7771cd9e1cbeb6f8808dbbdf315b90b6627f77
Robert Stoica <robe3750@stud.kea.dk>
2023-05-26 07:59:01 +0200

    added checks

--- src/pages/components/Modal.js
+++ src/pages/components/Modal.js
@@ -2,13 +2,41 @@ import Image from "next/image";
 import styles from "./Modal.module.css";
 
 // Modal.js
+// const Modal = ({ band, onClose }) => (
+//   <div className={styles.modal}>
+//     <div className={styles.modalContent}>
+//       <h2>{band.name}</h2>
+//       <p>{band.bio}</p>
+//       <p>Genre: {band.genre}</p>
+//       {band.logo && (
+//         <>
+//           <Image
+//             src={
+//               band.logo.startsWith("https")
+//                 ? band.logo
+//                 : `https://hollow-glowing-gladiolus.glitch.me/logos/${band.logo}`
+//             }
+//             alt={band.name}
+//             width={500}
+//             height={300}
+//           />
+//           {band.logoCredits && <p>Logo credits: {band.logoCredits}</p>}
+//         </>
+//       )}
+//       <button onClick={onClose}>Close</button>
+//     </div>
+//   </div>
+// );
+
+// export default Modal;
+
 const Modal = ({ band, onClose }) => (
   <div className={styles.modal}>
     <div className={styles.modalContent}>
-      <h2>{band.name}</h2>
-      <p>{band.bio}</p>
-      <p>Genre: {band.genre}</p>
-      {band.logo && (
+      <h2>{band && band.name}</h2>
+      <p>{band && band.bio}</p>
+      <p>Genre: {band && band.genre}</p>
+      {band && band.logo && (
         <>
           <Image
             src={
@@ -27,5 +55,3 @@ const Modal = ({ band, onClose }) => (
     </div>
   </div>
 );
-
-export default Modal;
